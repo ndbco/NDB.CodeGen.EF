17ee3f8dd66b9a5c64ad7270eb8ae0c2971ce02a
Ilyasin <ilyasin7700@gmail.com>
2025-12-25 08:17:39 +0700

    update template kelupaan

--- src/Implementations/DefaultCrudTemplateProvider.cs
+++ src/Implementations/DefaultCrudTemplateProvider.cs
@@ -7,11 +7,21 @@ public sealed class DefaultCrudTemplateProvider : ICrudTemplateProvider
 {
     public IReadOnlyList<CrudTemplate> GetTemplates() => new List<CrudTemplate>()
     {
+        new("Active","Command", "Active{{Entity}}Handler.cs", "Active.tpl",CrudType.Command),
         new("Add","Command","Add{{Entity}}Handler.cs","Add.tpl",CrudType.Command),
         new("Edit","Command","Edit{{Entity}}Handler.cs","Edit.tpl",CrudType.Command),
         new("Delete","Command","Delete{{Entity}}Handler.cs","Delete.tpl",CrudType.Command),
-        new("GetById","Query","Get{{Entity}}ByIdHandler.cs","GetById.tpl",CrudType.Query),
-        new("GetList","Query","Get{{Entity}}ListHandler.cs","GetList.tpl",CrudType.Query),
-        new("API","","{{Entity}}Controller.cs","API.tpl",CrudType.API),
+
+        new("Export", "Query",   "Export{{Entity}}Handler.cs", "Export.tpl",CrudType.Query),
+        new("GetById", "Query",   "Get{{Entity}}ByIdHandler.cs", "GetById.tpl",CrudType.Query),
+        new("GetList", "Query",   "Get{{Entity}}ListHandler.cs","GetList.tpl",CrudType.Query),
+        new("GetPaged","Query",   "Get{{Entity}}PagedHandler.cs","GetPaged.tpl",CrudType.Query),
+
+        new("ExportDto", "Object",  "{{Entity}}ExportDto.cs",       "ExportDto.tpl",CrudType.Query),
+        new("Request", "Object",  "{{Entity}}Request.cs",       "Request.tpl", CrudType.Command),
+        new("Response","Object",  "{{Entity}}Response.cs",      "Response.tpl", CrudType.Query),
+        new("Contract","Object",  "{{Entity}}Contract.cs",      "Contract.tpl", CrudType.Query),
+
+        new("API","",  "{{Entity}}Controller.cs","API.tpl", CrudType.API),
     };
 }
